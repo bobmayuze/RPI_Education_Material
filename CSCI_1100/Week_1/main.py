e3088b92b4c15a556e204760ec102af1627239ab
bobmayuze <584653629@qq.com>
2016-09-01 14:49:27 -0400

    Update
    Just update

---
+++ CSCI_1100/Week_1/main.py
@@ -0,0 +1,7 @@
+pi = 3.14159
+radius = 2
+height = 10
+base_area = pi * radius ** 2
+volume = base_area * height
+surface_area = 2 * base_area + 2 * pi *radius * height
+print("The volume is", volume, "and the surface area is", surface_area)
