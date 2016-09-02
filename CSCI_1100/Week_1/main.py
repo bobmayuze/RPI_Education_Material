92618ff56e8e02784a684384d2fd9e0736b577de
bobmayuze <584653629@qq.com>
2016-09-01 22:07:05 -0400

    Update
    Update

--- CSCI_1100/Week_1/main.py
+++ CSCI_1100/Week_1/main.py
@@ -1,7 +1,42 @@
-pi = 3.14159
-radius = 2
-height = 10
-base_area = pi * radius ** 2
-volume = base_area * height
-surface_area = 2 * base_area + 2 * pi *radius * height
-print("The volume is", volume, "and the surface area is", surface_area)
+# pi = 3.14159
+# radius = 2
+# height = 10
+# base_area = pi * radius ** 2
+# volume = base_area * height
+# surface_area = 2 * base_area + 2 * pi *radius * height
+# print("The volume is", volume, "and the surface area is", surface_area)
+
+
+
+
+# principal = 100
+# rate = 0.05
+# year = 0
+# principal = principal + rate * principal
+# year += 1
+# print("After", year,"years, I got $", principal )
+# principal = principal + rate * principal
+# year += 1
+# print("After", year,"years, I got $", principal )
+# principal = principal + rate * principal
+# year += 1
+# print("After", year,"years, I got $", principal )
+
+
+# Exercise 1
+Celsius = 64
+Fahrenheit = Celsius * 9/5 + 32
+print (Fahrenheit)
+
+# Exercise 2 
+length = 16.5
+width = 12.5
+height = 5
+volume = length*width*height
+volume
+area = 2*(length*width + width*height + height*length)
+area
+
+# Exercise 3
+256
+-8
