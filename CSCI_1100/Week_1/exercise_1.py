ba91f4f3d6855e75173b7e45fe224f7aa31e651d
bobmayuze <584653629@qq.com>
2016-09-05 15:32:38 -0400

    Update
    Make sure I can pass exercise 1 and 2 at least, as for the 3rd one,
    there is bug for developers to fix.

---
+++ CSCI_1100/Week_1/exercise_1.py
@@ -0,0 +1,3 @@
+Celsius = 64
+Fahrenheit = Celsius * 9/5 + 32
+print (Fahrenheit)
---
+++ CSCI_1100/Week_1/exercise_2.py
@@ -0,0 +1,7 @@
+length = 16.5
+width = 12.5
+height = 5
+volume = length*width*height
+print("volume =", volume)
+area = 2*(length*width + width*height + height*length)
+print("area =", area)
