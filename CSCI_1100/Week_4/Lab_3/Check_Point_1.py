83baf562f435aaa533e4f608f2be12941c992572
bobmayuze <584653629@qq.com>
2016-09-21 10:10:06 -0400

    Update
    Update

--- CSCI_1100/Week_4/Lab_3/Check_Point_1.py
+++ CSCI_1100/Week_4/Lab_3/Check_Point_1.py
@@ -18,16 +18,14 @@ def line_length(x1,y1,x2,y2):
     length = math.sqrt(length)
     return length
 
-
+# Initialize all variables needed and input variables
 initial_x = 10
 initial_y = 10
 next_x = float(input("The next x value ==> "))
 next_y = float(input("The next y value ==> "))
-
-print("The line has moved from ({0:d},{1:d}) ".format(initial_x, initial_y),\
-    "to", str(next_x), ",", next_y)
-
-
 total_length = line_length(initial_x, initial_y, next_x, next_y)
 
+
+# output the resulets
+print("The line has moved from ({0:d},{1:d}) ".format(initial_x, initial_y),"to (", next_x, ",", next_y,")", sep = '')
 print("Total length traveled is",round(total_length))
--- CSCI_1100/Week_4/Lab_3/Check_Point_3.py
+++ CSCI_1100/Week_4/Lab_3/Check_Point_3.py
@@ -2,19 +2,21 @@
 # Date: Sept, 19th, 2016
 # This code is made for Lab 3, Checkpoint 3
 
+
+# Declaer variables
 bpop = int(input('Number of bunnies ==> '))
 print(bpop)
 fpop = int(input('Number of foxs ==> '))
 print(fpop)
 
 
-
+# Define functions called for calculation
 def population_of_bunnies_for_the_next_year(bpop,fpop):
-	bpop_next = (10*bpop)/(1+0.1*bpop) - 0.05*bpop*fpop
+	bpop_next = max((10*bpop)/(1+0.1*bpop) - 0.05*bpop*fpop,0)
 	return bpop_next 
 
 def population_of_foxes_for_the_next_year(bpop,fpop):
-	fpop_next = 0.4 * fpop + 0.02 * fpop * bpop
+	fpop_next = max(0.4 * fpop + 0.02 * fpop * bpop,0)
 	return fpop_next
 
 print('Yaer 1:', bpop, fpop)
@@ -37,5 +39,5 @@ print('Yaer 4:', bpop_1, fpop_1)
 bpop = int(population_of_bunnies_for_the_next_year(bpop_1,fpop_1))
 fpop = int(population_of_foxes_for_the_next_year(bpop_1,fpop_1))
 
-print('Yaer 3:', bpop, fpop)
+print('Yaer 5:', bpop, fpop)
 
