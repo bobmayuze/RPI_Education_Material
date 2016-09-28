08f9487d0eacddac6cdc901538188fa7a8fb9d05
bobmayuze <584653629@qq.com>
2016-09-28 10:50:03 -0400

    Update
    Update

---
+++ CSCI_1100/Week_5/Test_1/main.py
@@ -0,0 +1,19 @@
+# avg = (2 + 3 + 5 + 6) / 3.0
+# s ="The average of {}, {:d}, {:d}, and {:d} is: {}" .format(1,2,5,6,avg)
+# print(s)
+
+def log_f(s1,s2):
+	L = max(len(s1),len(s2))
+	print('*',s1+'!'*(L - len(s1)), '*')
+	print('*',s2+'!'*(L - len(s2)), '*')
+	return(L)
+
+print(log_f('butwegwegfwter','caajhbfkqwhefbkqwhebckquwhevfquesf'))
+
+# s = 'fun' + 'ny'
+# print(s.replace('nn','N'))
+# s = 'cart ' + ' wheel'
+# print(s.replace('tw', 'X'))
+# t = '''hello """\nworld'''
+# print(len(t))
+# print(t  )
